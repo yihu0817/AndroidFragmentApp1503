677f88d8761a72c6a451f064b8441190b57b24b5
viktor.zhou <2915858910@qq.com>
2015-12-03 09:24:50 +0800

    Fragmetn交互

---
+++ app/src/main/java/com/scxh/android1503/fragment/parmeter/ParamentFramgentToActivity.java
@@ -0,0 +1,15 @@
+package com.scxh.android1503.fragment.parmeter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.scxh.android1503.R;
+
+public class ParamentFramgentToActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parament_framgent_to_layout);
+    }
+}

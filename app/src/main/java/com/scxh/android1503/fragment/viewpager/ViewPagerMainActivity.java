2b760509a5b611d8a5d9545ca55d9491fc558995
viktor.zhou <2915858910@qq.com>
2015-12-09 14:57:54 +0800

    SlidingmenuFragment

--- app/src/main/java/com/scxh/android1503/fragment/viewpager/ViewPagerMainActivity.java
+++ app/src/main/java/com/scxh/android1503/fragment/viewpager/ViewPagerMainActivity.java
@@ -2,28 +2,43 @@ package com.scxh.android1503.fragment.viewpager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.scxh.android1503.R;
 import com.scxh.android1503.fragment.tab.TabOneFragment;
 import com.scxh.android1503.fragment.tab.TabThreeFragment;
 import com.scxh.android1503.fragment.tab.TabTwoFragment;
+import com.scxh.android1503.slidemenu.MenuFragment;
+import com.warmtel.slidingmenu.lib.SlidingMenu;
+import com.warmtel.slidingmenu.lib.app.SlidingActivity;
 
 import java.util.ArrayList;
 
-public class ViewPagerMainActivity extends FragmentActivity {
+public class ViewPagerMainActivity extends SlidingActivity implements MenuFragment.MenuFragmentListener{
     private ViewPager mViewPager;
     private RadioGroup mRadioGroup;
     private RadioButton mOneRadioBtn,mTwoRadioBtn,mThreeRadioBtn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager_main_layout);
+
+        /**侧滑菜单界面*/
+        setBehindContentView(R.layout.sliding_menu_layout);
+        getSupportFragmentManager().beginTransaction().add(R.id.sliding_menu_contaner_layout, MenuFragment.newInstance()).commit();
+        SlidingMenu sm = getSlidingMenu();
+        sm.setSlidingEnabled(true);
+        sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setBehindScrollScale(0);
+        sm.setFadeDegree(0.25f);
+
         mViewPager = (ViewPager) findViewById(R.id.fragment_viewpager);
         mRadioGroup = (RadioGroup) findViewById(R.id.tab_radio_group);
         mOneRadioBtn = (RadioButton) findViewById(R.id.tab_radio_one);
@@ -91,6 +106,21 @@ public class ViewPagerMainActivity extends FragmentActivity {
         data.add(TabOneFragment.newInstance());
         data.add(new TabTwoFragment());
         data.add(TabThreeFragment.newInstance());
+    }
+
+    @Override
+    public void toNewFragment() {
+        Toast.makeText(ViewPagerMainActivity.this,getString(R.string.sliding_menu_news_fragment),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void toYuleFragment() {
+        Toast.makeText(ViewPagerMainActivity.this,getString(R.string.sliding_menu_yule_fragment),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void toKejiFragment() {
+        Toast.makeText(ViewPagerMainActivity.this,getString(R.string.sliding_menu_keji_fragment),Toast.LENGTH_SHORT).show();
     }
 
     public class MyFragmentPagerViewAdatper extends FragmentPagerAdapter {
